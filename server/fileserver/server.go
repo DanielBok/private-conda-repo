01ccf50b8226befc7326819201dd2852306d5355
Daniel Bok <daniel.bok@outlook.com>
2019-12-25 17:14:16 +0800

    Add fileserver

---
+++ server/fileserver/server.go
@@ -0,0 +1,53 @@
+package fileserver
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+
+	"private-conda-repo/config"
+)
+
+type router struct {
+	*chi.Mux
+}
+
+func New() (*http.Server, error) {
+	conf, err := config.New()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not start repository server due to issue with config")
+	}
+	addr := fmt.Sprintf(":%d", conf.RepoServer.Port)
+	log.WithField("Address", addr).Info("Server details")
+
+	r := router{chi.NewRouter()}
+	r.attachMiddleware()
+	r.addFileServer(conf)
+
+	return &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}, nil
+}
+
+func (r *router) attachMiddleware() {
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+}
+
+func (r *router) addFileServer(conf *config.AppConfig) {
+	root := http.Dir(conf.Conda.MountFolder)
+	fs := http.FileServer(root)
+
+	log.WithField("Repository mount folder used to serve packages", root).Info()
+
+	r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	})
+}

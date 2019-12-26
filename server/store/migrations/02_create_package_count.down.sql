95cb0dc90dd7e605676e37077c5abe2e0afc941c
Daniel Bok <daniel.bok@outlook.com>
2019-12-26 10:34:25 +0800

    Add PackageCount migrations

---
+++ server/store/migrations/02_create_package_count.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_package_counts__channel_package;
+DROP INDEX IF EXISTS idx_package_counts__channel_package_platform;
+DROP TABLE IF EXISTS package_count;
---
+++ server/store/migrations/02_create_package_count.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS package_counts
+(
+    id           SERIAL PRIMARY KEY,
+    channel      VARCHAR(50) NOT NULL UNIQUE,
+    package      VARCHAR(64) NOT NULL,
+    platform     VARCHAR(10) NOT NULL,
+    build_string VARCHAR(64) NOT NULL,
+    build_number INT         NOT NULL DEFAULT 0,
+    version      VARCHAR(30) NOT NULL,
+    count        INT         NOT NULL DEFAULT 0
+);
+
+CREATE INDEX idx_package_counts__channel_package on package_counts
+    (channel, package);
+
+CREATE INDEX idx_package_counts__channel_package_platform on package_counts
+    (channel, package, platform);

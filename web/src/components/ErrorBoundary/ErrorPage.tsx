be63ec5b2d0af336aa3fe211b448985be10c33a5
Daniel Bok <daniel.bok@outlook.com>
2019-12-29 12:43:22 +0800

    Redesign error page

--- web/src/components/ErrorBoundary/ErrorPage.tsx
+++ web/src/components/ErrorBoundary/ErrorPage.tsx
@@ -1,20 +1,25 @@
+import { Button, Typography } from "antd";
 import { push } from "connected-react-router";
 import React from "react";
-import styles from "./styles.less";
 import { useDispatch } from "react-redux";
 import { withRouter } from "react-router-dom";
+import NotFound from "./NotFound.png";
+import styles from "./styles.less";
 
 const ErrorPage = () => {
   const dispatch = useDispatch();
 
   return (
     <div className={styles.errorPage}>
-      <h1 className={styles.errorTitle}>
-        The page you're looking for does not exist
-      </h1>
-      <div className={styles.errorButton} onClick={() => goto("/")}>
-        Take me back to Homepage
-      </div>
+      <img src={NotFound} alt="Page not found" />
+      <h2>The page you're looking for does not exist</h2>
+      <Typography.Text>
+        Do you have permissions to view this page?
+      </Typography.Text>
+
+      <Button type="link" onClick={() => goto("/")}>
+        Return home
+      </Button>
     </div>
   );
 

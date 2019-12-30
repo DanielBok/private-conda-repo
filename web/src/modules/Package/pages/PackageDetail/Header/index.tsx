f5b4f75b91c27c6cbb3ed0faf5c7a4dfa9c62db7
Daniel Bok <daniel.bok@outlook.com>
2019-12-30 10:13:51 +0800

    Add package summary to PackageDetail header

--- web/src/modules/Package/pages/PackageDetail/Header/index.tsx
+++ web/src/modules/Package/pages/PackageDetail/Header/index.tsx
@@ -1,13 +1,19 @@
+import { PackageSelector } from "@/features/package";
 import React from "react";
+import { useSelector } from "react-redux";
 import { usePackageContext } from "../hooks";
 import styles from "./styles.less";
 
 export default () => {
   const { channel, pkg } = usePackageContext();
+  const { summary } = useSelector(PackageSelector.packageDetail).latest;
 
   return (
     <div className={styles.title}>
-      <span className={styles.channel}>{channel}</span> / {pkg}
+      <div>
+        <span className={styles.channel}>{channel}</span> / {pkg}
+      </div>
+      {summary && <div className={styles.subtitle}>{summary}</div>}
     </div>
   );
 };

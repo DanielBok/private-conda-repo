084e50b098121d67cc4d26c61c96f0daed8e9822
Daniel Bok <daniel.bok@outlook.com>
2020-02-28 18:15:50 +0800

    Set migrations from file source

--- server/store/postgres/migrate.go
+++ server/store/postgres/migrate.go
@@ -2,9 +2,14 @@ package postgres
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/golang-migrate/migrate/v4/source/github"
 	"github.com/pkg/errors"
 )
@@ -14,12 +19,10 @@ func (s *Store) Migrate() error {
 	if err != nil {
 		return errors.Wrap(err, "could not create database driver")
 	}
-
-	username := "danielbok"
-	publicRepoReadonlyToken := ""
-	repo := "private-conda-repo"
-	folderPath := "server/store/migrations"
-	sourceUrl := fmt.Sprintf("github://%s:%s@%s/%s/%s", username, publicRepoReadonlyToken, username, repo, folderPath)
+	sourceUrl, err := getMigrationSourceUrl()
+	if err != nil {
+		return err
+	}
 
 	m, err := migrate.NewWithDatabaseInstance(sourceUrl, "postgres", driver)
 	if err != nil {
@@ -30,4 +33,43 @@ func (s *Store) Migrate() error {
 		return errors.Wrap(err, "could not apply migrations")
 	}
 	return nil
+}
+
+func getMigrationSourceUrl() (string, error) {
+	pathExists := func(fp string) bool {
+		_, err := os.Stat(fp)
+		return !os.IsNotExist(err)
+	}
+
+	formatFolderPath := func(folder string) string {
+		sourceUrl := "file://" + folder
+		if runtime.GOOS == "windows" {
+			cwd, _ := os.Getwd()
+			sourceUrl = strings.Replace(strings.Replace(sourceUrl, cwd, ".", 1), `\`, "/", -1)
+		}
+		return sourceUrl
+	}
+
+	// search from source executable (which is usually the case for Docker images
+	root, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	mgDir := filepath.Join(filepath.Dir(root), "store", "migrations")
+	if pathExists(mgDir) {
+		return formatFolderPath(mgDir), nil
+	}
+
+	// search from local file path, (which is usually the case during development)
+	_, file, _, _ := runtime.Caller(0)
+	mgDir = filepath.Join(filepath.Dir(file), "..", "migrations")
+	if pathExists(mgDir) {
+		return formatFolderPath(mgDir), nil
+	}
+
+	username := "danielbok"
+	publicRepoReadonlyToken := ""
+	repo := "private-conda-repo"
+	folderPath := "server/store/migrations"
+	return fmt.Sprintf("github://%s:%s@%s/%s/%s", username, publicRepoReadonlyToken, username, repo, folderPath), nil
 }

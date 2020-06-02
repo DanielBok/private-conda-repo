18cc3c2f14c88c7f574731075286dac2dc9c61d0
Daniel Bok <daniel.bok@outlook.com>
2020-06-02 13:58:07 +0800

    Add version command

--- cli/main.go
+++ cli/main.go
@@ -17,15 +17,24 @@ var rootCmd = &cobra.Command{
 Aids in various aspect of using the Private Conda Repository
 application. This tool is catered for package contributors.
 `,
-	Version: "2.2",
+	Version: "3.0",
 }
 
 func main() {
 	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 
-	rootCmd.AddCommand(config.RootCmd, registry.RootCmd, upload.RootCmd)
+	rootCmd.AddCommand(config.RootCmd, registry.RootCmd, upload.RootCmd, versionCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number",
+	Long:  `All software has versions. This is Private Conda Repo CLI's`,
+	Run: func(cmd *cobra.Command, _ []string) {
+		cmd.Printf("Private Conda Repo CLI: %s", rootCmd.Version)
+	},
 }
